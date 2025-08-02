c82aa180bb1e400af9c0bf797e8e6ac89480ff40
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-08-02 14:50:42 -0300

    Agrego logica de mover objetos (de manera manual)

--- basics-practice/other-practices/main.py
+++ basics-practice/other-practices/main.py
@@ -1,34 +1,44 @@
 import pygame
 
-# Inicializar los atributos etc de pygame
+# 1. Inicializar los atributos etc de pygame
 pygame.init()
 
-# Set the height and width of the window screen as a tuple
+# 2. Set the height and width of the window screen as a tuple
 screen = pygame.display.set_mode((800,600))     # w,h
 
 ##### Events: anything that is happening inside the game window
 
-# Title and Icon
-pygame.display.set_caption("Space Invadors")    # Cambiar el titulo de la ventana (en la esquina superior izquierda)
+# 3. Title and Icon
+pygame.display.set_caption("Space Invader")    # Cambiar el titulo de la ventana (en la esquina superior izquierda)
 icon = pygame.image.load('icon-window.png')
 pygame.display.set_icon(icon)
 
-## How to add images into the game
+## 5. How to add images into the game
 # Jugador
 jugador_img = pygame.image.load('player.png')
 ## posicion (coordenadas) donde estara la imagen segun las dimensiones del screen
 jugadorX = 370
 jugadorY = 480
 
-def jugador():
+# 6. Movement of an object
+def jugador(x,y):
     # syntax: destination_surface.blit(source_surface, (x,y))
-    screen.blit(jugador_img,(jugadorX, jugadorY))   # blit means 'to draw'
+    screen.blit(jugador_img,(x, y))   # blit means 'to draw'
 
-# Game Loop
+
+# 4. Game Loop
 running = True
 while running:
     # RGB: Red, Green, Blue
     screen.fill((0,0,50))
+
+    '''
+    Movimiento manual
+    jugadorX += 0.1
+    jugadorX -= 0.1
+    jugadorY += 0.1
+    jugadorY -= 0.1
+    '''
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,7 +46,7 @@ while running:
 
 
     # se hace el llamado despues del screen.fill porque ese es el 'lienzo' donde estara el jugador
-    jugador()
+    jugador(jugadorX,jugadorY)
 
     # Cada vez que se hagan cambios al game hay que actualizarlo
     pygame.display.update()     # !! importante
